48fe91a1fd48dbb1aa26906550b68873b0c395c9
Sophie Lee <44243849+gahyunlee@users.noreply.github.com>
2019-03-07 14:00:51 -0600

    Update DataCamp_pjt1.py

--- DataCamp_pjt1.py
+++ DataCamp_pjt1.py
@@ -1,5 +1,6 @@
-### trying to make dictionary from a dataframe using iteration (for loop)
-
+### trying to make a dictionary from a dataframe using iteration (for loop)
+### the dictrionary has each word inside a column of a dataframe as a key 
+### and the number of its occurrence as its value.
 
 import os
 import pandas as pd 
@@ -7,7 +8,7 @@ cwd = os.getcwd()
 print(cwd) # to check where I am
 os.chdir("/Users/Sophie/Downloads/suicide-rates-overview-1985-to-2016")
 print(cwd) # to check I successfuly changed my working directory 
-df = pd.read_csv('master.csv')
+df = pd.read_csv('master.csv') # read my csv dataset as a dataframe
 langs_count = {}
 col = df['country']
 for entry in col:
